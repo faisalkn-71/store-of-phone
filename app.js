999af06cc4f03db7d46176d1e249081201b9eea9
faisalkn-71 <csust2021@gmail.com>
2022-02-28 21:06:29 +0600

    load-phone

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("app.js is connected")
+// load phone
+
+const loadPhone = () => {
+    fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`)
+    .then(res => res.json())
+    .then(info => displayPhone(info.data))
+}
+
+loadPhone()
+
+
+// display phone
+
+const displayPhone = (phones) => {
+    // console.log(phones)
+
+    phones.forEach(phone => {
+        console.log(phone)
+    })
+
+}
