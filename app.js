2f7795123a70c11e2b9daa6252b1d5c597c5b263
faisalkn-71 <csust2021@gmail.com>
2022-03-01 17:50:43 +0600

    display-20-phone-done

--- app.js
+++ app.js
@@ -40,29 +40,39 @@ const loadPhone = (searchText) => {
 // display phone
 
 const displayPhone = (phones) => {
-    // console.log(phones)
+    console.log(phones)
 
     // clear error massage
     document.getElementById("error").innerText = ''
     
+    const detailsContainer = document.getElementById("phone-details");
+    detailsContainer.textContent = ''
+
+    // phone container section
     const phoneContainer = document.getElementById("phone-container");
     phoneContainer.textContent = "";
-
-    phones.forEach(phone => {
+    
+    // show 20 phones in display
+    const showPhones = phones.slice(0, 20);
+    // console.log(showPhones)
+    
+    showPhones.forEach(phone => {
         // console.log(phone)
 
+
+        // innerHTML div for phone
         const div = document.createElement("div")
         div.classList.add("col")
 
         div.innerHTML = `
             
             <div class="card h-100 p-4 shadow">
-            <img width=200px class="mx-auto p-3" src=${phone.image}>
+            <img width=200px class="img-fluid mx-auto p-3" src=${phone.image}>
             <div class="card-body">
               <h5 class="card-title">${phone.phone_name}</h5>
               <p class="card-text">Brand: ${phone.brand}</p>
             </div>
-            <a onclick="loadPhoneDetails('${phone.slug}')" href="#phone-details" class="btn bg-primary text-white fw-bold rounded-pill">Phone Details</a>
+            <a onclick="loadPhoneDetails('${phone.slug}')" href="#phone-details" class="btn bg-primary text-white fw-bold rounded-pill">Details</a>
              
         `
         phoneContainer.appendChild(div)
@@ -85,39 +95,40 @@ const displayPhoneDetails = (detail) => {
     // console.log(detail)
     // console.log(detail.mainFeatures.sensors)
 
+    // phone details section
     const detailsContainer = document.getElementById("phone-details");
     detailsContainer.textContent = ''
 
     
-    
+    // phone sensors
     const sensor = detail.mainFeatures.sensors
-    console.log( ...sensor);
+    // console.log( ...sensor);
 
-
-
+    // innerHTML div for phone details
 
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
     <div class="card h-100 p-4 shadow">
-    <img width=300px class="mx-auto p-3" src=${detail.image}>
+    <img width=300px class="img-fluid mx-auto p-3" src=${detail.image}>
     <div class="card-body">
-      <h5 class="card-title">${detail.name}</h5>
+      <h4 class="card-title">${detail.name}</h4>
       <p class="card-text">Brand: ${detail.brand}</p>
       <p class="card-text">${detail.releaseDate ? detail.releaseDate : "Release Date: Unknown"}</p>
       <p class="card-text">Display Size: ${detail.mainFeatures.displaySize}</p>
       <p class="card-text">Storage: ${detail.mainFeatures.storage}</p>
       <p class="card-text">Memory: ${detail.mainFeatures.memory}</p>
       <p class="card-text">Chip set: ${detail.mainFeatures.chipSet}</p>
+
       <p class="card-text"><h5>Sensors:</h5> ${sensor}</p>
       
       <div class="card-text"><h5>Others</h5>
-      <p>Bluetooth: ${detail.others.Bluetooth}</p>
-      <p>GPS: ${detail.others.GPS}</p>
-      <p>NFC: ${detail.others.NFC}</p>
-      <p>Radio: ${detail.others.Radio}</p>
-      <p>USB: ${detail.others.USB}</p>
-      <p>WLAN: ${detail.others.WLAN}</p>
+      <p>Bluetooth: ${detail.others ? detail.others.Bluetooth: "unknown" }</p>
+      <p>GPS: ${detail.others? detail.others.GPS:"unknown" }</p>
+      <p>NFC: ${detail.others? detail.others.NFC: "unknown" }</p>
+      <p>Radio: ${detail.others? detail.others.Radio: "unknown" }</p>
+      <p>USB: ${detail.others? detail.others.USB: "unknown"}</p>
+      <p>WLAN: ${detail.others? detail.others.WLAN: "unknown"}</p>
     </div>
       
   </div>
